007de28fc8b54229cd85f215979ae61df8e22005
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-15 18:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstChar(text) {
+function firstChar(s) {
   // your code here
+	s=s.trimm();
+	let p=""
+	if(s.indexOf(0)!=0){
+		return s.indexOf(0);
+	}else{
+		return p;
+	}
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+//const text = prompt("Enter text:");
+//alert(firstChar(text));
